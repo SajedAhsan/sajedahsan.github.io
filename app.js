88ecac24507d7aa90aee845e372eb792440a2228
SajedAhsan <91411894+SajedAhsan@users.noreply.github.com>
2021-09-26 15:46:27 +0600

    Added new files

---
+++ app.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('#subbtn');
+const submitMsg = document.querySelector('.submitted');
+
+function clicked(){
+    submitMsg.innerHTML = "Succesfully Submitted!!!!";
+}
