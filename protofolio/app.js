0d258d827c0d4fc9743c5cc12271fc9efd0c922c
SajedAhsan <91411894+SajedAhsan@users.noreply.github.com>
2021-09-27 14:11:03 +0600

    updated one

--- protofolio/app.js
+++ protofolio/app.js
@@ -1,6 +1,21 @@
 const btn = document.querySelector('#subbtn');
 const submitMsg = document.querySelector('.submitted');
+const name = document.querySelector('#name') ;
+const message = document.querySelector('#message');
+const email = document.querySelector('#email');
+const nr = document.querySelector('#nr');
+const mr = document.querySelector('#mr');
+const er = document.querySelector('#er');
 
 function clicked(){
-    submitMsg.innerHTML = "Succesfully Submitted!!!!";
-}
+    if(name.value == '' || email.value == '' || message.value == ''){
+
+        alert("Please fill the form");
+    }else{
+        submitMsg.innerHTML = "Succesfully Submitted!!!!";
+    nr.innerHTML = name.value ;
+    er.innerHTML = email.value;
+    mr.innerHTML = message.value;
+    document.querySelector(".formReply").classList.add("disform");
+    }
+}   
